10684c1e43e8ff3ff1b29c18f464f5ceb31ffd1c
Muhammad Arifin <muhammad.arifin@nri.co.id>
2023-06-13 13:07:25 +0700

    chore: initial project setup & commits

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vitest/config";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/test/setup.ts",
+    coverage: {
+      provider: "istanbul",
+    },
+  },
+});
